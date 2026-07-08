9351cfdb0226169a591a45909e564f1e7b87dc28
ghdduq3-ops <ghdduq3@gmail.com>
2026-07-08 17:14:26 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sohee-more-characters-cars-v1';
+const CACHE='sohee-zoom-controls-v1';
 const FILES=['./','./index.html','./manifest.json','./icon.svg','./cartoon_puzzle_bgm.wav'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
