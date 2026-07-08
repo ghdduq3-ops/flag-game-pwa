336c0fb9d9b744d90e80f3a26f64f53432ce6f12
ghdduq3-ops <ghdduq3@gmail.com>
2026-07-08 17:07:06 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
-const CACHE_NAME = "flag-coloring-layout-v19";
-const FILES_TO_CACHE = ["./", "./index.html?v=19", "./index.html", "./manifest.json", "./flag_bgm_exciting.wav", "./icons/icon.svg"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(names => Promise.all(names.map(name => {
-    if (name !== CACHE_NAME) return caches.delete(name);
-  }))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE='sohee-more-characters-cars-v1';
+const FILES=['./','./index.html','./manifest.json','./icon.svg','./cartoon_puzzle_bgm.wav'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
